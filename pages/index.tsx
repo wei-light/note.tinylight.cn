cb06f7910ddf498de7f111e1890b8c5c22c23ef3
刘炜亮 <10076232+weilight@user.noreply.gitee.com>
2022-12-03 21:35:10 +0800

    feat: add unocss

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,12 @@
 export default function Home() {
-  return <h2>Home</h2>
+  return (
+    <button
+      text="sm white"
+      bg="blue-400 hover:blue-500 dark:blue-500 dark:hover:blue-600"
+      p="y-2 x-4"
+      border="2 rounded blue-500"
+    >
+      Button
+    </button>
+  )
 }
