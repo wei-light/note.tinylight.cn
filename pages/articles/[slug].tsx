9184e248fd85a48d85f7c3c9089c5e2d852b4fea
刘炜亮 <10076232+weilight@user.noreply.gitee.com>
2022-12-06 13:45:34 +0800

    feat: improve article page

--- pages/articles/[slug].tsx
+++ pages/articles/[slug].tsx
@@ -1,4 +1,6 @@
+import Image from 'next/image'
 import { useMemo } from 'react'
+import dayjs from 'dayjs'
 import { getMDXComponent } from 'mdx-bundler/client'
 import { getAllFileSlugs, loadMDXFile } from '~/lib/mdx'
 import { removeFileSuffix } from '~/lib/utils/file-utils'
@@ -17,9 +19,38 @@ const Article = ({ code, frontmatter }: InferGetServerSidePropsType<typeof getSt
   return (
     <>
       <section className="pt-8 pb-10">
+        {frontmatter.cover && (
+          <div className="relative overflow-hidden h-auto mb-9 aspect-[17/9] rounded-xl">
+            <Image
+              alt="cover"
+              src={frontmatter.cover}
+              fill
+              className="object-cover"
+            />
+          </div>
+        )}
         <h2 className="mt-6 mb-4 text-3xl font-bold sm:text-4xl">
           {frontmatter.title}
         </h2>
+        <div className="flex items-center text-sm">
+          <a href="https://github.com/wei-light">
+            <Image
+              alt="avatar"
+              src="/avatar.jpg"
+              width={24}
+              height={24}
+              quality={100}
+              className="rounded-full"
+            />
+          </a>
+          <p className="space-x-1 ml-4 opacity-40">
+            <time dateTime={frontmatter.date}>
+              {dayjs(frontmatter.date).format('MMMM DD, YYYY')}
+            </time>
+            <i>{'·'}</i>
+            <span>{frontmatter.duration}</span>
+          </p>
+        </div>
       </section>
       <article className="prose">
         <MDXContent />
