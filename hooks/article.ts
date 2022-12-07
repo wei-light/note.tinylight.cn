a2f73fcf74e16a227bd140ceea0406ca92724448
刘炜亮 <10076232+weilight@user.noreply.gitee.com>
2022-12-07 12:00:02 +0800

    perf: add useHeadingAnchors hook

---
+++ hooks/article.ts
@@ -0,0 +1,65 @@
+import { useEffect } from 'react'
+import { getElementTop } from '~/lib/utils/dom-utils'
+
+function useHeadingAnchors() {
+  useEffect(() => {
+    const navigate = () => {
+      if (location.hash) {
+        const el = document.querySelector(decodeURIComponent(location.hash))
+        if (el) {
+          const top = getElementTop(el)
+
+          window.scrollTo({
+            top: top - 48,
+            left: 0,
+            behavior: 'smooth',
+          })
+        }
+      }
+    }
+
+    const handleAnchors = (
+      e: MouseEvent,
+    ) => {
+      const link = (e.target as HTMLElement).closest('a')
+
+      if (link && !link.download) {
+        const { origin, pathname, hash, target } = link
+
+        // only intercept internal links
+        if (
+          !e.ctrlKey
+          && !e.shiftKey
+          && !e.altKey
+          && !e.metaKey
+          && target !== '_blank'
+          && origin === window.location.origin
+        ) {
+          e.preventDefault()
+          // TODO: Handle internal links that jump to different pages
+          if (pathname === window.location.pathname) {
+            // scroll between hash anchors in the same page
+            if (hash && hash !== window.location.hash) {
+              window.history.replaceState({}, '', hash)
+              navigate()
+            }
+          }
+        }
+      }
+    }
+
+    window.addEventListener('hashchange', navigate)
+    window.addEventListener('click', handleAnchors)
+    // run when page apper
+    setTimeout(navigate, 500)
+
+    return () => {
+      window.removeEventListener('hashchange', navigate)
+      window.removeEventListener('click', handleAnchors)
+    }
+  }, [])
+}
+
+export {
+  useHeadingAnchors,
+}
--- pages/articles/[...slug].tsx
+++ pages/articles/[...slug].tsx
@@ -1,11 +1,11 @@
-import { useEffect, useMemo, useRef } from 'react'
+import { useMemo } from 'react'
 import { getMDXComponent } from 'mdx-bundler/client'
 import ArticleHeader from '~/components/ArticleHeader'
+import { useHeadingAnchors } from '~/hooks/article'
 import { getAllFileSlugs, loadMDXFile } from '~/lib/mdx'
 import { splitPath } from '~/lib/utils/file-utils'
 
 import type { GetStaticPaths, InferGetServerSidePropsType } from 'next'
-import { getElementTop } from '~/lib/utils/dom-utils'
 
 type Params = {
   params: {
@@ -18,56 +18,13 @@ const Article = (
   InferGetServerSidePropsType<typeof getStaticProps>,
 ) => {
   const MDXContent = useMemo(() => getMDXComponent(code), [code])
-  const articleRef = useRef<HTMLElement>(null)
 
-  useEffect(() => {
-    const ref = articleRef.current!
-
-    const navigate = () => {
-      if (location.hash) {
-        const el = document.querySelector(decodeURIComponent(location.hash))
-        if (el) {
-          const top = getElementTop(el)
-
-          window.scrollTo({
-            top: top - 48,
-            left: 0,
-            behavior: 'smooth',
-          })
-        }
-      }
-    }
-
-    const handleAnchors = (
-      e: MouseEvent,
-    ) => {
-      const link = (e.target as HTMLElement).closest('a.anchor')
-        || (e.target as HTMLElement).closest('a[data-footnote-ref="true"]')
-        || (e.target as HTMLElement).closest('a[data-footnote-backref="true"]')
-
-      if (link) {
-        e.preventDefault()
-        const hash = link.getAttribute('href')
-        history.replaceState({}, '', hash)
-        navigate()
-      }
-    }
-
-    window.addEventListener('hashchange', navigate)
-    ref.addEventListener('click', handleAnchors)
-    // run when page apper
-    setTimeout(navigate, 500)
-
-    return () => {
-      window.removeEventListener('hashchange', navigate)
-      ref.removeEventListener('click', handleAnchors)
-    }
-  }, [])
+  useHeadingAnchors()
 
   return (
     <>
       <ArticleHeader title={title} date={date} duration={duration} cover={cover} />
-      <article ref={articleRef} className="prose">
+      <article className="prose">
         <MDXContent />
       </article>
     </>
