f7a5fb54a78a7cd342973b701d75833aeb2dc6dd
刘炜亮 <10076232+weilight@user.noreply.gitee.com>
2022-12-11 20:55:49 +0800

    fix: type error

--- src/pages/posts.tsx
+++ src/pages/posts.tsx
@@ -3,10 +3,10 @@ import AppLink from '~/components/AppLink'
 import { getAllFrontMatter } from '~/lib/mdx'
 import { sortDesc } from '~/lib/utils/business-utils'
 
-import type { GetStaticProps, InferGetServerSidePropsType } from 'next'
+import type { GetStaticProps, InferGetStaticPropsType } from 'next'
 import type { ArticleFrontMatter } from '~/types/shared'
 
-const PostsPage = ({ postsList }: InferGetServerSidePropsType<typeof getStaticProps>) => {
+const PostsPage = ({ postsList }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <PageSEO title="Posts - Tiny Light" />
